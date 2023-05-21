cd19e7e9d162f2d4b58556b559eb5d20e8065126
Jacky Huang <lashhw81@gmail.com>
2023-05-21 14:07:57 +0800

    w

--- cv_assert.py
+++ cv_assert.py
@@ -3,7 +3,7 @@ import glob
 for ans_path in sorted(glob.glob('ans/t_cv_*.txt')):
     print(f'checking {ans_path}')
     with open(ans_path) as f:
-        for line in f.readlines()[:5]:
+        for line in f.readlines():
             line = line.split()
             excess3 = int(line[-3], 2)
             bcd = int(line[-1], 2)
