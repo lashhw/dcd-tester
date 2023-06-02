1bf63f0b7fbd65cce834499b401ec81668d8cbef
Jacky Huang <lashhw81@gmail.com>
2023-06-02 21:13:42 +0800

    update `checker.py`

--- checker.py
+++ checker.py
@@ -2,10 +2,6 @@ import glob
 import os
 import sys
 
-YELLOW = '\033[93m'
-GREEN ='\033[92m' 
-DEFAULT = '\033[0m'
-
 for ans_path in sorted(glob.glob(f'{sys.argv[1]}/*.txt')):
     stu_path = os.path.basename(ans_path)
     with open(ans_path) as f:
@@ -14,5 +10,5 @@ for ans_path in sorted(glob.glob(f'{sys.argv[1]}/*.txt')):
         stu = f.readlines()
     same_lines = sum(1 for x, y in zip(ans, stu) if x == y)
     total_lines = len(ans)
-    color = GREEN if same_lines == total_lines else YELLOW
-    print(f'{color}{stu_path}: {same_lines}/{total_lines} ({same_lines/total_lines:.4}){DEFAULT}')
+    percentage = 'correct' if same_lines == total_lines else same_lines/total_lines
+    print(f'{stu_path}: {same_lines}/{total_lines} ({percentage})')
