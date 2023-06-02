664affa4d48fb5eed0a3ba14e65fef09cf7e63d5
Jacky Huang <lashhw81@gmail.com>
2023-06-02 20:59:52 +0800

    add `unzip.sh`

---
+++ unzip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="zip"
+LOG="${PWD}/log/log.txt"
+
+rm -f "${LOG}"
+cd "${DIR}" || exit 1
+for dir in *; do (
+    {
+        cd "${dir}" || exit 1
+        echo "=====[${dir}]====="
+        unzip *.zip
+        echo ""
+    } >> "${LOG}" 2>&1 
+) done
