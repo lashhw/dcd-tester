e9ff9b20ff5e8db838544531fa698308a20a1786
Jacky Huang <lashhw81@gmail.com>
2023-06-05 22:38:31 +0800

    stricter check

--- checker.py
+++ checker.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import sys
+import filecmp
 
 for ans_path in sorted(glob.glob(f'{sys.argv[1]}/*.txt')):
     try:
@@ -11,7 +12,7 @@ for ans_path in sorted(glob.glob(f'{sys.argv[1]}/*.txt')):
             stu = f.readlines()
         same_lines = sum(1 for x, y in zip(ans, stu) if x == y)
         total_lines = len(ans)
-        percentage = 'correct' if same_lines == total_lines else same_lines/total_lines
+        percentage = 'correct' if filecmp.cmp(ans_path, stu_path, shallow=False) else same_lines/total_lines
         print(f'{stu_path}: {same_lines}/{total_lines} ({percentage})')
     except FileNotFoundError:
         print(f'{stu_path}: file not found')
