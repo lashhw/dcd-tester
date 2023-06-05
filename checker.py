1f9d87b1541eb5200bf06ce9950c54dbd22da4e2
Jacky Huang <lashhw81@gmail.com>
2023-06-05 22:27:41 +0800

    report error when file not found

--- checker.py
+++ checker.py
@@ -3,12 +3,15 @@ import os
 import sys
 
 for ans_path in sorted(glob.glob(f'{sys.argv[1]}/*.txt')):
-    stu_path = os.path.basename(ans_path)
-    with open(ans_path) as f:
-        ans = f.readlines()
-    with open(stu_path) as f:
-        stu = f.readlines()
-    same_lines = sum(1 for x, y in zip(ans, stu) if x == y)
-    total_lines = len(ans)
-    percentage = 'correct' if same_lines == total_lines else same_lines/total_lines
-    print(f'{stu_path}: {same_lines}/{total_lines} ({percentage})')
+    try:
+        stu_path = os.path.basename(ans_path)
+        with open(ans_path) as f:
+            ans = f.readlines()
+        with open(stu_path) as f:
+            stu = f.readlines()
+        same_lines = sum(1 for x, y in zip(ans, stu) if x == y)
+        total_lines = len(ans)
+        percentage = 'correct' if same_lines == total_lines else same_lines/total_lines
+        print(f'{stu_path}: {same_lines}/{total_lines} ({percentage})')
+    except FileNotFoundError:
+        print(f'{stu_path}: file not found')
